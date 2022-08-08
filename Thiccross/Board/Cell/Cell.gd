effaaf7f3ce8c75efc89c7d6e44d7bf18eb4fea8
Joseph Myers <josephlmyers@gmail.com>
2022-08-08 11:35:34 -0500

    Cell logic complete. Need better unit tests for checking textures.

--- Thiccross/Board/Cell/Cell.gd
+++ Thiccross/Board/Cell/Cell.gd
@@ -4,10 +4,25 @@ var _is_dirty := false
 var _is_on := false
 var _is_excluded := false
 
+onready var white_square_texture := ImageTexture.new()
+onready var black_square_texture := ImageTexture.new()
+onready var excluded_square_texture := ImageTexture.new()
+onready var white_square_image := Image.new()
+onready var black_square_image := Image.new()
+onready var excluded_square_image := Image.new()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	white_square_image.load("res://Thiccross/Board/Cell/assets/white_square.png")
+	white_square_texture.create_from_image(white_square_image)
+	black_square_image.load("res://Thiccross/Board/Cell/assets/black_square.png")
+	black_square_texture.create_from_image(black_square_image)
+	excluded_square_image.load("res://Thiccross/Board/Cell/assets/excluded.png")
+	excluded_square_texture.create_from_image(excluded_square_image)
+	
+	texture_normal = white_square_texture
+	
 func get_is_dirty() -> bool:
 	return _is_dirty
 	
@@ -18,11 +33,34 @@ func get_is_on() -> bool:
 	return _is_on
 
 func toggle_is_on() -> void:
+	check_is_dirty_and_set()
 	_is_on = !_is_on
+	if(_is_on):
+		texture_normal = black_square_texture
+	else:
+		texture_normal = white_square_texture 
 
 func get_is_excluded() -> bool:
 	return _is_excluded
 
 func toggle_is_excluded() -> void:
+	check_is_dirty_and_set()
 	_is_excluded = !_is_excluded
+	if(_is_excluded):
+		texture_normal = excluded_square_texture
+	else:
+		texture_normal = white_square_texture
 
+func check_is_dirty_and_set():
+	if(!_is_dirty):
+		set_is_dirty()
+
+func _on_Cell_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		match event.button_index:
+			BUTTON_LEFT:
+				if not(get_is_excluded()):
+					toggle_is_on()
+			BUTTON_RIGHT:
+				if not(get_is_on()):
+					toggle_is_excluded()
--- test/unit/test_cell.gd
+++ test/unit/test_cell.gd
@@ -25,6 +25,11 @@ func test_is_on_toggles():
 	cell.toggle_is_on()
 	assert_true(cell.get_is_on())
 
+func test_is_on_toggles_sets_dirty():
+	var cell = add_child_autofree(Cell.instance())
+	cell.toggle_is_on()
+	assert_true(cell.get_is_dirty())
+
 func test_is_excluded_is_false_by_default():
 	var cell = autofree(Cell.instance())
 	assert_false(cell.get_is_excluded())
@@ -33,3 +38,27 @@ func test_is_excluded_toggles():
 	var cell = autofree(Cell.instance())
 	cell.toggle_is_excluded()
 	assert_true(cell.get_is_excluded())
+	
+func test_is_excluded_toggles_sets_dirty():
+	var cell = add_child_autofree(Cell.instance())
+	cell.toggle_is_excluded()
+	assert_true(cell.get_is_dirty())
+	
+	# TODO: try to figure out how to ensure it's loading correct resource
+func test_texture_normal_default_set():
+	var cell = add_child_autofree(Cell.instance())
+	assert_not_null(cell.texture_normal)
+	
+	# TODO: must figure out above todo to test these things
+func test_texture_changes_to_black_square_if_white_and_left_clicked():
+	pass
+
+func test_texture_doesnt_change_to_black_if_excluded_and_left_clicked():
+	pass
+
+func test_texture_changes_to_excluded_if_white_and_right_clicked():
+	pass
+
+func test_texture_doesnt_change_to_excluded_if_black_and_right_clicked():
+	pass
+	
